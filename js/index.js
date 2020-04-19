0e0ad7131f7ecd1c1b00ce63912db1917d2e8dc8
lucky20160622 <1048656160@qq.com>
2020-04-19 21:51:06 +0800

    1

--- js/index.js
+++ js/index.js
@@ -1,20 +1,14 @@
 // 顶部导航
+var windowTop=0
+$(window).scroll(function(){
+    let scrollS=$(this).scrollTop()
+    if(scrollS>=windowTop){
+        $('.header').slideUp();
+        windowTop=scrollS
+    }else{
+        $('.header').slideDown()
+        windowTop=scrollS
+    }
+})
 
-$(function(){  
-    var winHeight = $(document).scrollTop();
-    $(window).scroll(function() {
-    var scrollY = $(document).scrollTop();// 获取垂直滚动的距离，即滚动了多少
-    if (scrollY > 1){ //如果滚动距离大于1px则隐藏，
-    $('.header').addClass('hiddened');
-    // 否则删除隐藏类
-}else {
-    $('.header').removeClass('hiddened');
-}  
-if (scrollY > winHeight){ //如果没滚动到顶部，删除显示类，否则添加显示类
-    $('.header').removeClass('showed');
-} 
-else {
-    $('.header').addClass('showed');
-}        
-});
-});
+// 轮播图
