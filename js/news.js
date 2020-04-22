693ae2432f785886232a02e964e266465338c2b9
lucky20160622 <1048656160@qq.com>
2020-04-22 23:40:55 +0800

    1

---
+++ js/news.js
@@ -0,0 +1,12 @@
+// 顶部导航
+var windowTop=0
+$(window).scroll(function(){
+    let scrollS=$(this).scrollTop()
+    if(scrollS>=windowTop){
+        $('.header').slideUp();
+        windowTop=scrollS
+    }else{
+        $('.header').slideDown()
+        windowTop=scrollS
+    }
+})
